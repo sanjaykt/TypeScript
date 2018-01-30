d51733b6b1c39f72a24f0a54633643545a629169
Sanjay Tailor <st.sanjaytailor@gmail.com>
2018-01-30 10:44:17 +0530

    Create ArraysDS.ts

---
+++ ArraysDS.ts
@@ -0,0 +1,9 @@
+
+console.log("Arrays - DS from https://www.hackerrank.com/challenges/arrays-ds/problem");
+
+let arr = [1, 2, 3, 4, 5];
+let arrLenght = 5;
+
+if(arrLenght === arr.length){
+    console.log(arr.reverse());
+}
